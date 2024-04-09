05cdcab628c74fbf60cba0187b4dc8675a2c5c23
jglatts <john@z-axiscc.com>
2024-04-09 15:17:54 -0400

    get progressbar(delaycounter) implemented

--- ForceGaugeMachine/Form1.cs
+++ ForceGaugeMachine/Form1.cs
@@ -174,7 +174,8 @@ namespace ForceGaugeMachine
                 totalDeflection = Double.Parse(txtBoxTotalDeflection.Text);
                 deflectionInterval = Double.Parse(txtBoxDeflectionInterval.Text);
                 delayInterval = Double.Parse(txtBoxDelayInterval.Text);
-                progressBarTestInterval.Maximum = ((int)(delayInterval * 1000)) - 1;
+                //progressBarTestInterval.Maximum = ((int)(delayInterval * 1000)) - 1;
+                progressBarTestInterval.Maximum = ((int)(delayInterval * 100)) - 1;
                 progressBarTestInterval.Value = 0;
             }
             catch
